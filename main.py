3e674fb6a3e07f7548444dcc1e71a3a985e149a7
Kenny <kenny_oliver@icloud.com>
2021-03-21 18:05:22 +0000

    [+] Proof of concept

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 from VividHues import Clr
+import random
+#====================
+first_val = random.choice([True,False])
+second_val = random.choice([True,False])
+operators = ["AND","OR","XOR","NOT"]
+rand_operator = random.choice(operators)
+
+print(first_val,rand_operator,second_val)
+if rand_operator == "AND":
+  actual_result = first_val and second_val
+elif rand_operator == "OR":
+  actual_result = first_val or second_val
+elif rand_operator == "XOR":
+  actual_result = first_val ^ second_val
+elif rand_operator == "NOT":
+  actual_result = not second_val
+
+user_answer = input("Enter your answer\n\t--> ")
+
+if user_answer == str(actual_result):
+  print("YAY")
+else:
+  print("NO")
